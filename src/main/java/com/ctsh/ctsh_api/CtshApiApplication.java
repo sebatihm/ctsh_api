05b1ac3cdb96187ec3bc9cbbb3a3654bd0d082f0
Sebastian HdezMont <sebashermon003@gmail.com>
2026-09-11 23:52:32 -0600

    init: Repository creation

---
+++ src/main/java/com/ctsh/ctsh_api/CtshApiApplication.java
@@ -0,0 +1,13 @@
+package com.ctsh.ctsh_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtshApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtshApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ctsh/ctsh_api/CtshApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctsh.ctsh_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtshApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
